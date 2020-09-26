c6dded3c0a845750b93f2cd10e3d0d1dac6d88a9
Fatih Intekin <fatihint@gmail.com>
2020-09-27 02:03:16 +0300

    main flow refactored

--- main.py
+++ main.py
@@ -12,28 +12,39 @@ def main():
     parser.add_argument('-a', action='store_true', help='Analyze the given lyrics.')
     args = parser.parse_args()
 
+    def start_scrape(app):
+        scrape_validation_errors = Utils.scrape_validation()
+        if not scrape_validation_errors:
+            values = Utils.get_config_values('artists_input_file', 'lyrics_result_file')
+            app.scrape(values['artists'], values['lyrics'])
+        else:
+            for error in scrape_validation_errors:
+                print(error)
+                exit(0)
+
+    def start_analyze(app):
+        analyze_validation_errors = Utils.analyze_validation()
+        if not analyze_validation_errors:
+            values = Utils.get_config_values('lyrics_result_file', 'stats_result_file')
+            app.analyze(values['lyrics'], values['stats'])
+        else:
+            for error in analyze_validation_errors:
+                print(error)
+                exit(0)
+
     scrape, analyze = args.l, args.a
     app = App()
 
     if not scrape and not analyze:
-        # All 3 keys are needed.
-        scrape_validation_errors = Utils.scrape_validation()
-        analyze_validation_errors = Utils.analyze_validation()
-
-        if not scrape_validation_errors and not analyze_validation_errors:
-            values = Utils.get_config_values("artists_input_file", 'lyrics_result_file', 'stats_result_file')
-            app.scrape(values['artists'], values['lyrics'])
-            app.analyze(values['lyrics'], values['stats'])
-        else:
-            for error in scrape_validation_errors + analyze_validation_errors:
-                print(error)
+        # Default behavior. Both scrape and analyze.
+        start_scrape(app)
+        start_analyze(app)
     else:
+        # One or both of the arguments are given.
         if scrape:
-            values = Utils.get_config_values('artists_input_file', 'lyrics_result_file')
-            app.scrape(values)
+            start_scrape(app)
         if analyze:
-            values = Utils.get_config_values('lyrics_result_file', 'stats_result_file')
-            app.analyze(values)
+            start_analyze(app)
 
     sys.exit(0)
 
